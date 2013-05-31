e235bd22032f856ca6d70fb8fa7acfdb1966084b
Emmanuel Bernard <emmanuel@hibernate.org>
2013-05-31 22:43:57 +0200

    Add backbone structure to select a facet to filter by

--- demo/src/main/webapp/resources/js/app/models/results.js
+++ demo/src/main/webapp/resources/js/app/models/results.js
@@ -10,10 +10,19 @@ define([
      */
     var Results = Backbone.Model.extend({
         urlRoot: config.baseUrl + 'rest/search', // the URL for performing CRUD operations
+        initialize  : function() {
+            _.bindAll(this,"fetch");
+        },
         url: function() {
             params = '?query=' + encodeURIComponent(this.get("query"));
             if (typeof this.get("lat") != 'undefined' && typeof this.get("lng") != 'undefined') {
                 params = params + '&latitude=' + encodeURIComponent(this.get("lat")) + '&longitude=' + encodeURIComponent(this.get("lng"));
+            }
+            if (typeof this.get("category") != 'undefined') {
+                params = params + '&categoryfacet=' + this.get("category");
+            }
+            if (typeof this.get("price") != 'undefined') {
+                params = params + '&minpricefacet=' + this.get("price");
             }
             console.log("Calling REST endpoint " + this.urlRoot + params);
             return this.urlRoot + params;
--- demo/src/main/webapp/resources/js/app/views/desktop/results.js
+++ demo/src/main/webapp/resources/js/app/views/desktop/results.js
@@ -14,12 +14,27 @@ define([
 	var ResultsView  = Backbone.View.extend({
 
         events:{
+            "click .faceting": "enableFaceting"
+        },
+        
+        initialize:function () {
+            this.model.bind('change', this.render, this);
         },
         
         render:function () {
             $(this.el).empty();
+            this.delegateEvents();
             utilities.applyTemplate($(this.el), resultsTemplate, {model:this.model, query:this.model.get("query")});
             return this;	
+        },
+        
+        enableFaceting:function (e) {
+            var id = $(e.currentTarget).data("id");
+            var faceting = id.substring(0, id.lastIndexOf('-'));
+            var index = id.substring(id.lastIndexOf('-') + 1);
+            this.model.set(faceting, index);
+            this.model.fetch();
+            return false;
         }
     });
     return ResultsView;
